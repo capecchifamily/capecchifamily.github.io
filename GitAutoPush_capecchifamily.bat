4e26290e283443fa5eadce6622284e04232a8acc
capecchi <william.capecchi@gmail.com>
2017-12-05 10:32:30 -0600

    auto push

---
+++ GitAutoPush_capecchifamily.bat
@@ -0,0 +1,22 @@
+:: Navigate to the directory you wish to push to GitHub
+::Change <path> as needed. Eg. C:\Users\pookie\Desktop\Writings
+cd C:\Users\Owner\Documents\GitHub\capecchifamily.github.io\
+
+::Initialize GitHub
+git init
+
+::Pull any external changes (maybe you deleted a file from your repo?)
+git pull
+
+::Add all files in the directory
+git add --all
+
+::Commit all changes with the message "auto push".
+::Change as needed.
+git commit -m "auto push"
+
+::Push all changes to GitHub
+git push
+
+::Alert user to script completion and relaunch.
+echo Complete. Relaunching...
